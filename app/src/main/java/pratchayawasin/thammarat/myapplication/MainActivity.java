32ceaef0aff1feff108634db51bb0e47fbd088fa
winney <thammarath014@gmail.com>
2016-06-04 15:02:45 +0700

    Finish 04 June 2016

--- app/src/main/java/pratchayawasin/thammarat/myapplication/MainActivity.java
+++ app/src/main/java/pratchayawasin/thammarat/myapplication/MainActivity.java
@@ -1,14 +1,45 @@
 package pratchayawasin.thammarat.myapplication;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // ctrl + allt + L (reformat code)
+    //Explicit
+
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Bind Widget
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button); // alt + enter = Cast Data Type
+
+        //Controller button
+
+        controllerButton();
+
     }
+
+    private void controllerButton() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.lion);
+                mediaPlayer.start();
+            }// On Click
+        });
+    }// Controller Button
 
 }// Main Class
